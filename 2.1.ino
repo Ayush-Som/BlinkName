143d21e10a4e684664d7813ea46542f226bfc8f1
Ayush-Som <103960059+Ayush-Som@users.noreply.github.com>
2022-09-07 11:34:08 +0530

    Add files via upload

---
+++ 2.1.ino
@@ -0,0 +1,82 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, HIGH);   // Creating dot effect
+  delay(200);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(200);
+
+  digitalWrite(LED_BUILTIN, HIGH);   // Creating dash effect
+  delay(600);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(600);
+
+  digitalWrite(LED_BUILTIN, HIGH);   // Creating dash effect
+  delay(600);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(600);
+
+  digitalWrite(LED_BUILTIN, HIGH);   // Creating dot effect
+  delay(200);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(200);
+
+  digitalWrite(LED_BUILTIN, HIGH);   // Creating dash effect
+  delay(600);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(600);
+
+  digitalWrite(LED_BUILTIN, HIGH);   // Creating dash effect
+  delay(600);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(600);
+
+  digitalWrite(LED_BUILTIN, HIGH);   // Creating dot effect
+  delay(200);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(200);
+
+  digitalWrite(LED_BUILTIN, HIGH);   // Creating dot effect
+  delay(200);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(200);
+
+  digitalWrite(LED_BUILTIN, HIGH);   // Creating dash effect
+  delay(600);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(600);
+
+  digitalWrite(LED_BUILTIN, HIGH);   // Creating dot effect
+  delay(200);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(200);
+  digitalWrite(LED_BUILTIN, HIGH);   // Creating dot effect
+  delay(200);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(200);
+  digitalWrite(LED_BUILTIN, HIGH);   // Creating dot effect
+  delay(200);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(200);
+  digitalWrite(LED_BUILTIN, HIGH);   // Creating dot effect
+  delay(200);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(200);
+  digitalWrite(LED_BUILTIN, HIGH);   // Creating dot effect
+  delay(200);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(200);
+  digitalWrite(LED_BUILTIN, HIGH);   // Creating dot effect
+  delay(200);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(200);
+  digitalWrite(LED_BUILTIN, HIGH);   // Creating dot effect
+  delay(200);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(200);
+
+}
